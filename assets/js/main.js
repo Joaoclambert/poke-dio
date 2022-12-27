42e0f68b54ab7b7e95763e8fddf9edd7c1db5322
Joao Carlos <joaoclambert@hotmail.com>
2022-12-27 01:36:34 -0300

    projeto final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,18 @@
-console.log('Sucesso!')
+let offset = 0;
+let pagina = 0;
+const limit = 20;
 
+function avancarPagina() {
+    pagina = pagina + 1;
+    offset = pagina * limit;
+    const divLista = document.getElementById('lista-pokemons');
+    divLista.innerHTML = ""
+    main();
+}
 
 function main() {
 
-    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20").then(resposta => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`).then(resposta => {
         resposta.json().then(respostaObjetoJavascript => {
             console.log(respostaObjetoJavascript)
             const listaPokemons = respostaObjetoJavascript.results;
@@ -17,18 +26,28 @@ function main() {
                         const divLista = document.getElementById('lista-pokemons')
                         const htmlPokemon = `
                             <li class="pokemon ${respostaPokemonObjeto.types[0].type.name}">
-                            <span class="number">#${respostaPokemonObjeto.id}</span>
-                            <span class="name">${respostaPokemonObjeto.name}</span>
-                        
-                            <div class="detail">
-                                <ol class="types">
-                                    <li class="type">${respostaPokemonObjeto.types[0].type.name}</li>
-                                    ${respostaPokemonObjeto.types.length > 1 ? `<li class="type">${respostaPokemonObjeto.types[1].type.name}</li>` : ''}
-                                </ol>
-                        
-                                <img src="${respostaPokemonObjeto.sprites.front_default}"
-                                    alt="${respostaPokemonObjeto.name}">
-                            </div>
+                                <span class="number">#${respostaPokemonObjeto.id}</span>
+                                <span class="name primeira-maiuscula">${respostaPokemonObjeto.name}</span>
+
+                                <div class="detail">
+                                    <ol class="types">
+                                        <li class="type">${respostaPokemonObjeto.types[0].type.name}</li>
+                                        ${respostaPokemonObjeto.types.length > 1 ? `<li class="type">${respostaPokemonObjeto.types[1].type.name}</li>` : ''}
+                                    </ol>
+
+                                    <img src="${respostaPokemonObjeto.sprites.front_default}"
+                                        alt="${respostaPokemonObjeto.name}">
+                                </div>
+                                <div class="info-adicional">
+                                    <span><strong>HP:</strong> ${respostaPokemonObjeto.stats[0].base_stat}</span>
+                                    <span><strong>Attack:</strong> ${respostaPokemonObjeto.stats[1].base_stat}</span>
+                                    <span><strong>Defense:</strong> ${respostaPokemonObjeto.stats[2].base_stat}</span>
+                                    <span><strong>Special Attack:</strong> ${respostaPokemonObjeto.stats[3].base_stat}</span>
+                                    <span><strong>Special Defense:</strong> ${respostaPokemonObjeto.stats[4].base_stat}</span>
+                                    <span><strong>Speed:</strong> ${respostaPokemonObjeto.stats[5].base_stat}</span>
+                                    <span><strong>Base Experience:</strong> ${respostaPokemonObjeto.base_experience}</span>
+                                    <span class="primeira-maiuscula"><strong>Specie:</strong> ${respostaPokemonObjeto.species.name}</span>
+                                </div>
                             </li>
                         `
                         divLista.innerHTML = divLista.innerHTML + htmlPokemon
